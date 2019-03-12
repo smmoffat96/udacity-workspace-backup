e71e300d1392590cef002d9705329d3706aa4f91
Shannon Moffat <smmoffat@wpi.edu>
2019-03-12 20:27:57 +0000

    Mar 12 4:27pm

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/udacity-workspace-backup/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/udacity-workspace-backup/catkin_ws/build/first_package/cmake_install.cmake")
   include("/home/udacity-workspace-backup/catkin_ws/build/simple_arm/cmake_install.cmake")
 
 endif()
