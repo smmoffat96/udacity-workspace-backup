4a6c4135a2e45f143c413e9773e561944453abcf
Shannon Moffat <smmoffat@wpi.edu>
2019-03-24 22:44:26 +0000

    feat: Edit drive_bot.cpp some more...

--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -5,8 +5,8 @@
 // ROS::Publisher motor commands;
 ros::Publisher motor_command_publisher;
 
-// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
-// This function should publish the requested linear x and angular velocities to the robot wheel joints
+// Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+// This function should publish the requested linear x and angular z velocities to the robot wheel joints
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
 
 bool handle_drive_request(ball_chaser::DriveToTarget::Request &req,
