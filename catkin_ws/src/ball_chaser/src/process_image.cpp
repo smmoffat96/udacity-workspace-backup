ac264d3b07d54b5cacfe1f052b2468d20a800d42
Shannon Moffat <smmoffat@wpi.edu>
2019-03-25 14:48:31 +0000

    feat: Edit process_image.cpp 2

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -1,6 +1,11 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 // Define a global client that can request services
 ros::ServiceClient client;
@@ -14,11 +19,64 @@ void drive_robot(float lin_x, float ang_z)
     srv.request.angular_z = ang_z;
 }
 
+
+// Image converter
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/camera/image_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
+
+
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    int white_pixel_count = 0;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
@@ -31,10 +89,25 @@ void process_image_callback(const sensor_msgs::Image img)
             redval = rgbpix[2];
             blueval = rgbpix[0];
             greenval = rgbpix[1];
-            if (redval=255 && blueval=255 && greenval=255) {
-                
+            // Determine if the pixel is white
+            if (redval=white_pixel && blueval=white_pixel && greenval=white_pixel) {
+                white_pixel_count++;
+                // if left side of image, turn left
+                    // lin_x = 0.0;
+                    // ang_z = 0.5;
+                // if right side of image, turn right
+                    // lin_x = 0.0;
+                    // ang_z = -0.5;
+                // if middle of image, drive forward
+                    // lin_x = 0.5;
+                    // ang_z = 0.0;
             }
         }
+    }
+    // if no white pixels, stop driving
+    if (white_pixel_count == 0) {
+        lin_x = 0.0;
+        ang_z = 0.0;
     }
 
 }
@@ -43,6 +116,7 @@ int main(int argc, char** argv)
 {
     // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "process_image");
+    ImageConverter ic;
     ros::NodeHandle n;
 
     // Define a client service capable of requesting services from command_robot
