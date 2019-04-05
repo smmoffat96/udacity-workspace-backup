c1de0f664cd8efd07364a325410879a26064c90f
Shannon Moffat <47715738+smmoffat96@users.noreply.github.com>
2019-04-05 09:43:23 -0400

    feat: Update process_image.cpp with edits from Pepar

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -1,9 +1,8 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -19,63 +18,34 @@ void drive_robot(float lin_x, float ang_z)
     srv.request.angular_z = ang_z;
 }
 
-
-// Image converter
+// Name of OpenCV image
 static const std::string OPENCV_WINDOW = "Image window";
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1,
-      &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-    cv::namedWindow(OPENCV_WINDOW);
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
-};
-
-
-
+void view_image(const cv::Mat img) {
+    // This function takes in an OpenCV image in BGR color channels and outputs to a window
+    cv::namedWindwo(OPENCV_WINDOW);
+    cv::imshow(OPENCV_WINDOW, img);
+    cv::WaitKey(3);
+}
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-
+    float lin_x;
+    float ang_z;
+    cv::Vec3b rgbpix;
+    
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+    cv::Mat cv_img = cv_ptr->image;
+    
+    view_image(cv_img);
+    
+    // Initialize window parameters with placeholders
+    int height = 10;
+    int width = 10;
+    int step = 0;
+    
     int white_pixel = 255;
     int white_pixel_count = 0;
 
@@ -113,6 +83,7 @@ void process_image_callback(const sensor_msgs::Image img)
         lin_x = 0.0;
         ang_z = 0.0;
     }
+    // if lots of white pixels, stop driving
 
 }
 
@@ -120,7 +91,6 @@ int main(int argc, char** argv)
 {
     // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "process_image");
-    ImageConverter ic;
     ros::NodeHandle n;
 
     // Define a client service capable of requesting services from command_robot
