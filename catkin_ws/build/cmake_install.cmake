6b40f66eb93eebe3f1a655bd1838dcd0e1be985a
Shannon Moffat <smmoffat@wpi.edu>
2019-05-13 19:27:25 +0000

    feat: Set up map

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -118,6 +118,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/udacity-workspace-backup/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/udacity-workspace-backup/catkin_ws/build/my_robot/cmake_install.cmake")
+  include("/home/udacity-workspace-backup/catkin_ws/build/pgm_map_creator/cmake_install.cmake")
 
 endif()
 
