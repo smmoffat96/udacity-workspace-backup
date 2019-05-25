bbc7618ee372bbe993c4350a766b084d0c3e0805
Shannon Moffat <smmoffat@wpi.edu>
2019-05-25 02:50:35 +0000

    feat: Partial map of willow garage world

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
+subdirs(slam_gmapping/slam_gmapping)
 subdirs(turtlebot/turtlebot)
 subdirs(turtlebot/turtlebot_bringup)
 subdirs(turtlebot/turtlebot_capabilities)
@@ -12,5 +13,6 @@ subdirs(turtlebot_simulator/turtlebot_gazebo)
 subdirs(turtlebot_simulator/turtlebot_simulator)
 subdirs(turtlebot_simulator/turtlebot_stage)
 subdirs(turtlebot_simulator/turtlebot_stdr)
+subdirs(slam_gmapping/gmapping)
 subdirs(turtlebot/turtlebot_teleop)
 subdirs(turtlebot/turtlebot_description)
