df476354cbb584d206ef2d495f89f5e4f5e59b46
Shannon Moffat <smmoffat@wpi.edu>
2019-06-04 15:32:42 +0000

    feat: Create test_slam.sh script

---
+++ catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm -e " roslaunch turtlebot_simulator turtlebot_world.launch " &
+sleep 5
+xterm -e " rosrun slam_gmapping slam_gmapping " &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
+sleep 5
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
