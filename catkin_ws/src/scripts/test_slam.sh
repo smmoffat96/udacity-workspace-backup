bf2ea0790216117eb89b57e8dfc41a42cba21a46
Shannon Moffat <smmoffat@wpi.edu>
2019-06-04 15:42:36 +0000

    feat: Clone turtlebot_apps and edit script

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+xterm -e " source /home/udacity-workspace-backup/catkin_ws/devel/setup.bash " &
+sleep 5
 xterm -e " roslaunch turtlebot_simulator turtlebot_world.launch " &
 sleep 5
 xterm -e " rosrun slam_gmapping slam_gmapping " &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
 xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
