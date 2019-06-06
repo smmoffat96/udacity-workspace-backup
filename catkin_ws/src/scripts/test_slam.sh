30b0337738cd8ae8a07a55c1bca28452bd8af1ce
Shannon Moffat <smmoffat@wpi.edu>
2019-06-06 14:19:06 +0000

    feat: Edit test_slam.sh script

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 xterm -e " source /home/udacity-workspace-backup/catkin_ws/devel/setup.bash " &
 sleep 5
-xterm -e " gazebo " &
-sleep 5
 xterm -e " roslaunch turtlebot_simulator turtlebot_world.launch " &
 sleep 5
-xterm -e " roslaunch turtlebot_apps gmapping_demo.launch " &
+xterm -e " roslaunch turtlebot_navigation gmapping_demo.launch " &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
