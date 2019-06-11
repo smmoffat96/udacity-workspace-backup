7a3a403c1951c094cbb918681e27dd2e86aafff6
Shannon Moffat <smmoffat@wpi.edu>
2019-06-11 19:44:05 +0000

    feat: Catkin_make with slam_gmapping

---
+++ catkin_ws_p7_v1/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/udacity-workspace-backup/catkin_ws_p7_v1/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/udacity-workspace-backup/catkin_ws_p7_v1/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/udacity-workspace-backup/catkin_ws_p7_v1/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/udacity-workspace-backup/catkin_ws_p7_v1/src:$ROS_PACKAGE_PATH"
---
+++ catkin_ws_p7_v1/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/udacity-workspace-backup/catkin_ws_p7_v1/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
