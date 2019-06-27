d20ee64476156fc17761733f73841261c90276c1
Shannon Moffat <smmoffat@wpi.edu>
2019-06-27 14:45:43 +0000

    feat: Communicate with ros params attempt - in progress

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,56 +1,15 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-#include <geometry_msgs/Pose.h>
-#include <nav_msgs/Odometry.h>
-
-class Poses
-{
-public:
-    geometry_msgs::Pose goal;
-    geometry_msgs::Pose odom;
-
-    void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_pose);
-    void goalCallback(const geometry_msgs::Pose &goal_pose);
-};
-
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_pose) {
-    odom.position.x = odom_pose->pose.pose.position.x;
-    odom.position.y = odom_pose->pose.pose.position.y;
-    odom.orientation.w = odom_pose->pose.pose.orientation.w;
-    ROS_INFO("Odom Pose -> x: [%f], y: [%f], w: [%f]", 
-        odom_pose->pose.pose.position.x, odom_pose.pose.position.y,
-        odom_pose->pose.pose.orientation.w);
-}
-
-void goalCallback(const move_base_msgs::MoveBaseGoal &goal_pose) {
-    goal.position.x = goal_pose.target_pose.pose.position.x;
-    goal.position.y = goal_pose.target_pose.pose.position.y;
-    goal.orientation.w = goal_pose.target_pose.pose.orientation.w;
-
-}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "add_markers");
     ros::NodeHandle n;
     ros::Rate r(1);
     ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-    ros::Subscriber odom_sub = n.subscribe("odom_pose", 1, odomCallback);
-    ros::Subscriber goal_sub = n.subscribe("goal_pose", 1, goalCallback);
-    ros::spin();
 
-    // Odometry pose
-    float odom_pose_x = odom.position.x;
-    float odom_pose_y = odom.position.y;
-    float odom_pose_w = odom.orientation.w;
-    // Goal pose
-    float goal_pose_x = goalposition.x;
-    float goal_pose_y = goal.position.y;
-    float goal_pose_w = goal.orientation.w;
-
-    // Set our initial shape type to be a cube
-    uint32_t shape = visualization_msgs::Marker::CUBE;
-    uint32_t pick_del_drop = 0;
+    // Define ros params
+    std::string robot_pose;
+    geometry_msgs::Pose goal;
 
     while (ros::ok()) {
         visualization_msgs::Marker marker;
@@ -59,22 +18,23 @@ int main(int argc, char** argv) {
         marker.header.stamp = ros::Time::now();
         marker.ns = "add_markers";
         marker.id = 0;
-
-        marker.type = shape;
-
+        // Shape
+        marker.type = visualization_msgs::Marker::CUBE;
+        // Scale
         marker.scale.x = 0.5;
         marker.scale.y = 0.5;
         marker.scale.z = 0.5;
-
+        // Color
         marker.color.r = 0.0f;
         marker.color.g = 1.0f;
         marker.color.b = 0.0f;
         marker.color.a = 1.0;
 
         // Marker pose
-        marker.pose.position.x = goal_pose_x;
-        marker.pose.position.y = goal_pose_y;
-        marker.pose.orientation.w = goal_pose_w;
+        ros::param::get("/goal", goal);
+        marker.pose.position.x = goal.position.x;
+        marker.pose.position.y = goal.position.y;
+        marker.pose.orientation.w = goal.orientation.w;
 
         marker.pose.position.z = 0;
         marker.pose.orientation.x = 0;
@@ -83,69 +43,31 @@ int main(int argc, char** argv) {
 
         marker.lifetime = ros::Duration();
 
-        switch (pick_del_drop)
-        {
-        // Pick
-        case 0:
-            marker.action = visualization_msgs::Marker::ADD;
-
-            // Publish the marker
-            while (marker_pub.getNumSubscribers() < 1) {
-                if (!ros::ok()) {
-                    return 0;
-                }
-                ROS_WARN_ONCE("Please create a subscriber to the marker");
-                sleep(1);
+        // Publish the marker
+        while (marker_pub.getNumSubscribers() < 1) {
+            if (!ros::ok()) {
+                return 0;
             }
-            marker_pub.publish(marker);
-            ROS_INFO("Pickup");
-            
-
-            pick_del_drop = 1;
-
-            break;
-        // Delete
-        case 1:
-            marker.action = visualization_msgs::Marker::DELETE;
-
-            // Publish the marker
-            while (marker_pub.getNumSubscribers() < 1) {
-                if (!ros::ok()) {
-                    return 0;
-                }
-                ROS_WARN_ONCE("Please create a subscriber to the marker");
-                sleep(1);
-            }
-            marker_pub.publish(marker);
-            ROS_INFO("Delete");
-
-            pick_del_drop = 2;
-
-            break;
-        // Drop
-        case 2:
-            marker.action = visualization_msgs::Marker::ADD;
-
-            // Publish the marker
-            while (marker_pub.getNumSubscribers() < 1) {
-                if (!ros::ok()) {
-                    return 0;
-                }
-                ROS_WARN_ONCE("Please create a subscriber to the marker");
-                sleep(1);
-            }
-            marker_pub.publish(marker);
-            ROS_INFO("Dropoff");
-
-            pick_del_drop = 3;
-
-            break;
-        // End
-        case 3:
-            ROS_INFO("End");
-            break;
+            ROS_WARN_ONCE("Please create a subscriber to the marker");
+            sleep(1);
         }
 
+        marker.action = visualization_msgs::Marker::ADD;
+        marker_pub.publish(marker);
+
+        ros::param::get("/robot_pose", robot_pose);
+
+        if (robot_pose == "pickup_pose") {
+            // Leave in pickup pose for 5 seconds before hiding
+            ros::Duration(5.0).sleep();
+            marker.action = visualization_msgs::Marker::DELETE;
+            marker_pub.publish(marker);
+
+        }
+        if (robot_pose == "dropoff_pose") {
+            marker.action = visualization_msgs::Marker::ADD;
+            marker_pub.publish(marker);
+        }
         ros::Duration(5.0).sleep();
     }
 }
--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -1,8 +1,6 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include <geometry_msgs/Pose.h>
-#include <nav_msgs/Odometry.h>
 
 // Define a client to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -11,19 +9,18 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 int main(int argc, char** argv) {
     // Initialize the pick_objects node
     ros::init(argc, argv, "pick_objects");
-    ros::NodeHandle n;
-    ros::Rate r(1);
-    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry::pose>("odom_pose", 1);
-    ros::Publisher goal_pub = n.advertise<move_base_msgs::MoveBaseGoal>("goal_pose", 1);
 
     // Tell the action client that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
+    // Update robot position using rosparam
+    ros::param::set("/robot_pose", "init_pose");
     // Wait 5 sec for move_base action server to come up
     while(!ac.waitForServer(ros::Duration(5.0))) {
         ROS_INFO("Waiting for the move_base action server to come up");
     }
 
     move_base_msgs::MoveBaseGoal goal_pick;
+    ros::param::set("/goal", goal_pick);
     // Set up the frame parameters
     goal_pick.target_pose.header.frame_id = "map";
     goal_pick.target_pose.header.stamp = ros::Time::now();
@@ -40,15 +37,16 @@ int main(int argc, char** argv) {
     // Check if the robot reached its goal
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
         ROS_INFO("The robot reached the pickup zone");
+        ros::param::set("/robot_pose", "pickup_pose");
     else
         ROS_INFO("The robot failed to reach the pickup zone");
+        ros::param::set("/robot_pose", "pickup_pose_failed");
 
-    // Wait 5 sec for move_base action server to come up
-    while(!ac.waitForServer(ros::Duration(5.0))) {
-        ROS_INFO("Waiting for the move_base action server to come up");
-    }
+    // Wait 5 seconds
+    ros::Duration(5.0)::sleep();
 
     move_base_msgs::MoveBaseGoal goal_drop;
+    ros::param::set("/goal", goal_drop);
     // Set up the frame parameters
     goal_drop.target_pose.header.frame_id = "map";
     goal_drop.target_pose.header.stamp = ros::Time::now();
@@ -65,8 +63,10 @@ int main(int argc, char** argv) {
     // Check if the robot reached its goal
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
         ROS_INFO("The robot reached the dropoff zone");
+        ros::param::set("/robot_pose", "dropoff_pose");
     else
         ROS_INFO("The robot failed to reach the dropoff zone");
+        ros::param::set("/robot_pose", "dropoff_pose_failed");
 
     return 0;
 }
