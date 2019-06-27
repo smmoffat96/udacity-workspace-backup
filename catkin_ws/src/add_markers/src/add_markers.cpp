877e8f4b1b4bef68922177f67c7ffbf664c963f5
Shannon Moffat <smmoffat@wpi.edu>
2019-06-27 13:12:17 +0000

    feat: Update add_markers.cpp - in progress

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv) {
 
     // Set our initial shape type to be a cube
     uint32_t shape = visualization_msgs::Marker::CUBE;
+    uint32_t pick_del_drop = 0;
 
     while (ros::ok()) {
         visualization_msgs::Marker marker;
@@ -16,74 +17,106 @@ int main(int argc, char** argv) {
         marker.header.frame_id = "map";
         marker.header.stamp = ros::Time::now();
 
-        // Set the namespace and id for this marker
-        // Any marker sent with the same namespace and id will overwrite the old one
         marker.ns = "add_markers";
         marker.id = 0;
 
         marker.type = shape;
-        marker.action = visualization_msgs::Marker::ADD;
 
-        // Set the pose of the marker. This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0;
-        marker.pose.orientation.y = 0;
-        marker.pose.orientation.z = 0;
-        marker.pose.orientation.w = 1.0;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
         marker.scale.x = 0.5;
         marker.scale.y = 0.5;
         marker.scale.z = 0.5;
 
-        // Set the color -- be sure to set alpha to something non-zero!
         marker.color.r = 0.0f;
         marker.color.g = 1.0f;
         marker.color.b = 0.0f;
         marker.color.a = 1.0;
 
-        marker.lifetime = ros::Duration(5);
+        switch (pick_del_drop)
+        {
+        // Pick
+        case 0:
+            marker.action = visualization_msgs::Marker::ADD;
+            
+            // Pickup Pose
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0;
+            marker.pose.orientation.y = 0;
+            marker.pose.orientation.z = 0;
+            marker.pose.orientation.w = 1.0;
 
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1) {
-            if (!ros::ok()) {
-                return 0;
+            marker.lifetime = ros::Duration();
+
+            // Publish the marker
+            while (marker_pub.getNumSubscribers() < 1) {
+                if (!ros::ok()) {
+                    return 0;
+                }
+                ROS_WARN_ONCE("Please create a subscriber to the marker");
+                sleep(1);
             }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
-        }
-        marker_pub.publish(marker);
-        
-        // Hide the marker
-        //marker.action = visualization_msgs::Marker::DELETE;
+            marker_pub.publish(marker);
+            ROS_INFO("Pickup");
 
-        // Pause five seconds
-        //marker.lifetime = ros::Duration(5.0);
+            pick_del_drop = 1;
 
-        // Add the marker
-        //marker.action = visualization_msgs::Marker::ADD;
+            break;
+        // Delete
+        case 1:
+            marker.action = visualization_msgs::Marker::DELETE;
 
-        // Set the pose of the marker. This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 5;
-        marker.pose.position.y = -10;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0;
-        marker.pose.orientation.y = 0;
-        marker.pose.orientation.z = 0;
-        marker.pose.orientation.w = 1.0;
+            marker.lifetime = ros::Duration();
 
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1) {
-            if (!ros::ok()) {
-                return 0;
+            // Publish the marker
+            while (marker_pub.getNumSubscribers() < 1) {
+                if (!ros::ok()) {
+                    return 0;
+                }
+                ROS_WARN_ONCE("Please create a subscriber to the marker");
+                sleep(1);
             }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
+            marker_pub.publish(marker);
+            ROS_INFO("Delete");
+
+            pick_del_drop = 2;
+
+            break;
+        // Drop
+        case 2:
+            marker.action = visualization_msgs::Marker::ADD;
+
+            // Dropoff Pose
+            marker.pose.position.x = -5;
+            marker.pose.position.y = 0;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0;
+            marker.pose.orientation.y = 0;
+            marker.pose.orientation.z = 0;
+            marker.pose.orientation.w = 1.0;
+
+            marker.lifetime = ros::Duration();
+
+            // Publish the marker
+            while (marker_pub.getNumSubscribers() < 1) {
+                if (!ros::ok()) {
+                    return 0;
+                }
+                ROS_WARN_ONCE("Please create a subscriber to the marker");
+                sleep(1);
+            }
+            marker_pub.publish(marker);
+            ROS_INFO("Dropoff");
+
+            pick_del_drop = 3;
+
+            break;
+        // End
+        case 3:
+            ROS_INFO("End");
+            break;
         }
-        marker_pub.publish(marker);
-        */
+
         r.sleep();
     }
 }
